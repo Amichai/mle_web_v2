3576d4ba1a1dbc1352fca0b776a1bfe52f77fba1
Amichai Levy <amichailevy@Amichais-MacBook-Pro.local>
2024-04-08 22:25:23 -0400

    wip

--- src/components/SlateAdder.vue
+++ src/components/SlateAdder.vue
@@ -7,6 +7,7 @@ import Dropdown from 'primevue/dropdown';
 import { useDataManager } from '../composables/useDataManager';
 import { getTodaysDateString } from '../utils';
 import uploadIcon from '@/assets/upload.png'
+import Papa from 'papaparse';
 
   
 const props = defineProps({
@@ -24,6 +25,7 @@ const filteredRows = ref([])
 const selectedSlate = ref('')
 const slates = ref([])
 const reader = new FileReader();
+const filename = ref('')
 
 const emits = defineEmits(['gotFocus'])
 
@@ -48,7 +50,7 @@ const slateFileUploaded = (evt) => {
   emits('gotFocus', selectedSlate.value)
   const files = evt.target.files; // FileList object
   const f = files[0];
-  // const name = f.name;
+  filename.value = f.name;
 
   reader.onload = (() => {
     return function (e) {
@@ -96,35 +98,36 @@ const uploadSlateFile = () =>  {
                           <Dropdown v-model="selectedSlate" :options="slates" optionLabel="name" placeholder="Select a slate" class="w-full md:w-14rem dropdown" />
                         </div>
                     </div>
-                    <div class="flex pt-4 justify-content-end">
-                        <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="nextCallback" />
+                    <div class="flex pt-4 justify-content-end button-row">
+                      <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="nextCallback" />
+                      <Button label="Cancel" severity="secondary" @click="cancelSlate" />
                     </div>
                 </template>
             </StepperPanel>
-            <StepperPanel header="Upload Your Slate CSV File">
+            <StepperPanel header="Upload Your CSV File">
                 <template #content="{ prevCallback }">
                     <div class="flex flex-column h-12rem">
                         <div class="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">
                           <div class="input-file-row" v-show="(selectedSlate && !filteredRows.length) || true"
                           >
                           <div class="file-upload-wrapper">
-                            <Button class="button upload-button tooltip" 
+                            <Button class="button upload-button" 
                             severity="secondary"
                             @click="uploadSlateFile">
                               <img :src="uploadIcon" alt="upload projections" width="40">
-                              <span class="tooltiptext">
-                                Upload projections
-                              </span>
                             </Button>
                             <input class="form-control-projections" type="file" 
                             @change="slateFileUploaded"
                             id="formFileSlateFile">
+                            {{ filename }}
                           </div>
                           </div>
                         </div>
                     </div>
-                    <div class="flex pt-4 justify-content-between">
-                        <Button label="Back" severity="primary" icon="pi pi-arrow-left" @click="prevCallback" />
+                    <div class="flex pt-4 justify-content-between button-row">
+                      <Button label="Back" severity="primary" icon="pi pi-arrow-left" @click="prevCallback" />
+
+                      <Button label="Cancel" severity="secondary" @click="cancelSlate" />
                     </div>
                 </template>
             </StepperPanel>
@@ -139,5 +142,11 @@ const uploadSlateFile = () =>  {
 
 .form-control-projections {
   display: none;
+}
+
+.button-row {
+  display: flex;
+  gap: 1rem;
+  margin-top: 2rem;
 }
 </style>
